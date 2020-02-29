b9c986f23714a4f54a22cda9193234d74cbb1a93
Viraj Bharadwaj <40692590+Viraj12356@users.noreply.github.com>
2020-02-29 19:08:41 +0530

    Add files via upload

---
+++ Assignment 1/timing_one.py
@@ -0,0 +1,15 @@
+import pexpect
+import time
+import threading
+child_server = pexpect.spawn('./server')
+n=100
+l=[]
+for i in range(n):
+    l.append(pexpect.spawn('./client 8888 127.0.0.1'))
+l[n-10].expect('connected')
+start = time.time()
+l[n-10].sendline('hello')
+l[n-1].expect('hello')
+end = time.time()
+print(end - start)
+
